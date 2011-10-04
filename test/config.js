75302899622efea326f37732444d14d6dac32b65
Dominic Barnes <contact@dominicbarnes.us>
2011-10-04 01:44:51 -0500

    working on adding attachments support via the client

--- test/config.js
+++ test/config.js
@@ -10,7 +10,7 @@ module.exports = {
 		password: null,
 		ssl:      false
 	},
-	log_level: "warn",
+	log_level: "debug",
 	name: function (type) {
 		counter[type] = counter[type] || 0;
 		return "test_" + type + "_" + counter[type]++;
