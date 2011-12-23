2392b087104579fc7aa825703c81084c87a47c72
Dominic Barnes <contact@dominicbarnes.us>
2011-12-22 22:10:45 -0700

    ensuring all cases convert functions to strings, improving plural functions

--- test/ddoc.js
+++ test/ddoc.js
@@ -36,63 +36,126 @@ module.exports = {
         "Views": function (done) {
             test.ifError(ddoc.body.views); // make sure views doesn't exist on our new doc
 
-            var views = ddoc.views(),      // initialize the array and return the reference
+            var views = ddoc.views(),
                 viewName = "test_view_1",
-                ret = ddoc.view(viewName, noop);  // add a new view
+                ret = ddoc.view(viewName, noop);
 
-            test.ok(views[viewName].map);         // make sure the map function is defined
-            test.ifError(views[viewName].reduce); // make sure the reduce function is not defined
-            test.strictEqual(ddoc, ret);          // test to make sure this is chainable
+            test.ok(views[viewName].map);
+            test.ok(typeof views[viewName].map === "string");
+            test.ifError(views[viewName].reduce);
+            test.strictEqual(ddoc, ret);
 
             viewName = "test_view_2";
-            ret = ddoc.view(viewName, noop, noop); // add a new view
-            test.ok(views[viewName].map);          // make sure the map function is defined
-            test.ok(views[viewName].reduce);       // make sure the reduce function is not defined
-            test.strictEqual(ddoc, ret);           // test to make sure this is chainable
+            ret = ddoc.view(viewName, noop, noop);
+            test.ok(views[viewName].map);
+            test.ok(typeof views[viewName].map === "string");
+            test.ok(views[viewName].reduce);
+            test.ok(typeof views[viewName].reduce === "string");
+            test.strictEqual(ddoc, ret);
 
             test.strictEqual(ddoc.body.views, views);
+            test.equal(_.size(ddoc.body.views), 2);
+
+            ddoc.views(null);
+            test.ifError(ddoc.body.views);
+
+            views = {
+                myview: {
+                    map: noop,
+                    reduce: noop
+                }
+            };
+
+            ddoc.views(views);
+            test.ok(ddoc.body.views);                  // did it set the object?
+            test.ok(views.myview.map);
+            test.ok(typeof views.myview.map === "string");
+            test.ok(views.myview.reduce);
+            test.ok(typeof views.myview.reduce === "string");
+            test.strictEqual(ddoc, ret);
+            test.strictEqual(ddoc.body.views, views);  // is it the same?
+
             done();
         },
         "Shows": function (done) {
-            test.ifError(ddoc.body.shows); // make sure shows doesn't exist on our new doc
-            var shows = ddoc.shows(),      // initialize the array and return the reference
-                showName = "test_show_1",
-                ret = ddoc.show(showName, noop); // add a new show function
-            test.ok(shows[showName]);            // test for existance
-            test.strictEqual(ddoc, ret);         // test to make sure this is chainable
+            test.ifError(ddoc.body.shows);
 
+            var shows = ddoc.shows(),
+                showName = "test_show_1",
+                ret = ddoc.show(showName, noop);
+
+            test.ok(shows[showName]);
+            test.ok(typeof shows[showName] === "string");
+            test.equal(_.size(shows), 1);
+            test.strictEqual(ddoc, ret);
             test.strictEqual(ddoc.body.shows, shows);
+
+            ddoc.shows(null);
+            test.ifError(ddoc.body.shows);
+
+            shows = { myshow: noop };
+            ddoc.shows(shows);
+            test.strictEqual(ddoc.body.shows, shows);
+            test.ok(typeof shows.myshow === "string");
 
             done();
         },
         "Lists": function (done) {
-            test.ifError(ddoc.body.lists); // make sure lists doesn't exist on our new doc
-            var lists = ddoc.lists(),      // initialize the array and return the reference
-                listName = "test_list_1",
-                ret = ddoc.list(listName, noop); // add a new list function
-            test.ok(lists[listName]);            // test for existance
-            test.strictEqual(ddoc, ret);         // test to make sure this is chainable
+            test.ifError(ddoc.body.lists);
 
+            var lists = ddoc.lists(),
+                listName = "test_list_1",
+                ret = ddoc.list(listName, noop);
+
+            test.ok(lists[listName]);
+            test.ok(typeof lists[listName] === "string");
+            test.strictEqual(ddoc, ret);
+            test.strictEqual(ddoc.body.lists, lists);
+
+            ddoc.lists(null);
+            test.ifError(ddoc.body.lists);
+
+            lists = {
+                mylist: noop
+            };
+            ddoc.lists(lists);
+            test.ok(ddoc.body.lists);
+            test.ok(typeof lists.mylist === "string");
             test.strictEqual(ddoc.body.lists, lists);
 
             done();
         },
         "Update Handlers": function (done) {
-            test.ifError(ddoc.body.updates); // make sure updates doesn't exist on our new doc
-            var updates = ddoc.updates(),    // initialize the array and return the reference
-                updateName = "test_list_2",
-                ret = ddoc.update(updateName, noop); // add a new update handler
-            test.ok(updates[updateName]);            // test for existance
-            test.strictEqual(ddoc, ret);             // test to make sure this is chainable
+            test.ifError(ddoc.body.updates);
 
+            var updates = ddoc.updates(),
+                updateName = "test_list_2",
+                ret = ddoc.update(updateName, noop);
+
+            test.ok(updates[updateName]);
+            test.ok(typeof updates[updateName] === "string");
+            test.strictEqual(ddoc, ret);
+            test.strictEqual(ddoc.body.updates, updates);
+
+            ddoc.updates(null);
+            test.ifError(ddoc.body.updates);
+
+            updates = {
+                myupdate: noop
+            };
+            ddoc.updates(updates);
+            test.ok(ddoc.body.updates.myupdate);
+            test.ok(typeof updates.myupdate === "string");
             test.strictEqual(ddoc.body.updates, updates);
 
             done();
         },
         "Validation": function (done) {
-            test.ifError(ddoc.body.validate_doc_update); // make sure updates doesn't exist on our new doc
-            var ret = ddoc.val(noop);                    // set the validation function
-            test.strictEqual(ddoc, ret);                 // test to make sure this is chainable
+            test.ifError(ddoc.body.validate_doc_update);
+
+            var ret = ddoc.val(noop);
+            test.strictEqual(ddoc, ret);
+            test.ok(typeof ddoc.body.validate_doc_update === "string");
 
             test.strictEqual(ddoc.body.validate_doc_update, ddoc.val());
             done();
