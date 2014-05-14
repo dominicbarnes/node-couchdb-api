10a3e6cff4f95ecec4b40429eea003464b8b256a
Dominic Barnes <dbarnes@treetopllc.com>
2014-05-14 03:39:01 -0500

    adding a smart entry-point, which will return a server/database depending on the passed url

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-module.exports = require("./lib/server");
+// dependencies
+var url = require("url");
+var Server = require("./lib/server");
+
+/**
+ * Creates an instance based on the input URL. If only a protocol+host is provided, it will return a Server instance.
+ * If a path is included, it will return a database instance. (ignoring the rest of the path)
+ *
+ * If nothing is provided, it will return a Server using http://localhost:5984
+ *
+ * @param {String} [href]
+ * @returns {Server|Database}
+ */
+module.exports = exports = function (href) {
+    if (!href) return new Server();
+
+    var parsed = url.parse(href);
+
+    var srv = new Server(url.format({
+        protocol: parsed.protocol,
+        auth: parsed.auth,
+        hostname: parsed.hostname,
+        port: parsed.port
+    }));
+
+    var path = parsed.pathname.slice(1);
+    if (!path) return srv;
+
+    return srv.db(path.split("/").pop());
+};
+
+// expose the constructors
+exports.Server = Server;
+exports.Database = require("./lib/database");
+exports.Document = require("./lib/document");
+exports.DesignDocument = require("./lib/design-document");
+exports.LocalDocument = require("./lib/local-document");
+exports.View = require("./lib/view");
--- test/index.js
+++ test/index.js
@@ -1,14 +1,30 @@
 var expect = require("expect.js");
 var couchdb = require("..");
 var Server = require("../lib/server");
+var Database = require("../lib/database");
 
 describe("node-couchdb-api", function () {
     it("should be a function", function () {
         expect(couchdb).to.be.a("function");
     });
 
-    it("should return a server instance", function () {
-        var srv = couchdb("http://localhost:5984/");
+    it("should return a basic server instance", function () {
+        var srv = couchdb();
         expect(srv).to.be.a(Server);
+        expect(srv.client.url()).to.equal("http://localhost:5984/");
+    });
+
+    it("should return a correct server instance", function () {
+        var srv = couchdb("https://user:pass@www.example.com:3000/");
+        expect(srv).to.be.a(Server);
+        expect(srv.client.url()).to.equal("https://user:pass@www.example.com:3000/");
+    });
+
+    it("should return a correct database instance", function () {
+        var db = couchdb("https://user:pass@www.example.com:3000/_users");
+        expect(db).to.be.a(Database);
+        expect(db.name).to.equal("_users");
+        expect(db.client).to.equal(db.server.client);
+        expect(db.url()).to.equal("https://user:pass@www.example.com:3000/_users");
     });
 });
